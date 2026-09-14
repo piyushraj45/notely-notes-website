43c17cad305601c52a559fb0f184871d824e76be
piyushraj45 <rajpiyush82100@gmail.com>
2026-09-14 17:49:14 +0530

    fix model case small letter

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Note = require('../models/Note');
+const Note = require('../models/note');
 const auth = require('../middleware/auth');
 const router = express.Router();
 
